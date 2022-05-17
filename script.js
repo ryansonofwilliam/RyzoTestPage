94dd2f0b6da425cefb02824ca74e264525453f5a
ryansonofwilliam <61905463+ryansonofwilliam@users.noreply.github.com>
2022-05-18 00:19:19 +0100

    Added JS to add navbar Elements

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // simple script to inject elements to the contents menu
 
-function addMenuItem(item) {
-    var menuContainer =  document.querySelector('dropdowncontents')
+function addMenuItem(link, name) {
+    // 
+    var menuContainer =  document.querySelector('.dropdowncontents');
     
-    menuContainer.appendChild
+    //create a list element
+    var menuItem = document.createElement('li');
+    menuItem.setAttribute('class', "menuItem");
+    //create an anchor element and use function variables to populate
+    var menuLink =  document.createElement('a');
+    menuLink.setAttribute('href', link);
+    menuLink.innerHTML = name;
+    // add the menu items to the HTML
+    menuItem.appendChild(menuLink);
+    menuContainer.appendChild(menuLink)
+    // add new line
+    var newLine = document.createElement('br')
+    menuContainer.appendChild(newLine)
 }
+
+// Add new Contents here using the function:
+addMenuItem("./To_do_list_site/index.html", "To Do List")
+addMenuItem("#", "Placeholder 1")
+addMenuItem('#', "Placeholder 2")
