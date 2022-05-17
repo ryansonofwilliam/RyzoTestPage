59e1b72f7fdab5895d111b47af4283843951c0c1
ryansonofwilliam <61905463+ryansonofwilliam@users.noreply.github.com>
2022-05-17 21:24:23 +0100

    wip

---
+++ script.js
@@ -0,0 +1,7 @@
+// simple script to inject elements to the contents menu
+
+function addMenuItem(item) {
+    var menuContainer =  document.querySelector('dropdowncontents')
+    
+    menuContainer.appendChild
+}
