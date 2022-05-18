907f27acb7d15b797439be14a3e98172dc6c1f04
ryansonofwilliam <61905463+ryansonofwilliam@users.noreply.github.com>
2022-05-18 17:13:52 +0100

    functional stopwatch

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // simple script to inject elements to the contents menu
 
 function addMenuItem(link, name) {
-    var menuContainer =  document.querySelector('.dropdowncontents');
-    //create a list element
-    var menuItem = document.createElement('li');
-    menuItem.setAttribute('class', "menuItem");
-    //create an anchor element and use function variables to populate
-    var menuLink =  document.createElement('a');
-    menuLink.setAttribute('href', link);
-    menuLink.innerHTML = name;
-    // add the menu items to the HTML
-    menuItem.appendChild(menuLink);
-    menuContainer.appendChild(menuItem);
-    // add new line
-    // var newLine = document.createElement('br')
-    // menuContainer.appendChild(newLine)
+  var menuContainer = document.querySelector(".dropdowncontents");
+  //create a list element
+  var menuItem = document.createElement("li");
+  menuItem.setAttribute("class", "menuItem");
+  //create an anchor element and use function variables to populate
+  var menuLink = document.createElement("a");
+  menuLink.setAttribute("href", link);
+  menuLink.innerHTML = name;
+  // add the menu items to the HTML
+  menuItem.appendChild(menuLink);
+  menuContainer.appendChild(menuItem);
+  // add new line
+  // var newLine = document.createElement('br')
+  // menuContainer.appendChild(newLine)
 }
 
 // Add new Contents here using the function:
-addMenuItem("./To_do_list_site/index.html", "To Do List")
-addMenuItem("./stopwatch/stopwatch.html", "StopWatch")
-addMenuItem('#', "Placeholder 2")
+addMenuItem("./To_do_list_site/index.html", "To Do List");
+addMenuItem("./stopwatch/stopwatch.html", "StopWatch");
+addMenuItem("#", "Placeholder 2");
--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,46 +1,46 @@
-var container = document.getElementById("stop_watch_container")
-
+var container = document.getElementById("stop_watch_container");
+current_number = 00;
 //Set the Title
-var title = document.createElement('p')
-title.setAttribute('class', 'title')
-title.innerText = "Stopwatch"
-container.appendChild(title)
+var title = document.createElement("p");
+title.setAttribute("class", "title");
+title.innerText = "Stopwatch";
+container.appendChild(title);
 
 //Set the description
-var description = document.createElement('p')
-description.setAttribute('class', 'title')
-description.innerText = "Vanilla JS Stopwatch"
-container.appendChild(description)
+var description = document.createElement("p");
+description.setAttribute("class", "title");
+description.innerText = "Vanilla JS Stopwatch";
+container.appendChild(description);
 
 //Create a counter container and contents
-var counter_container = document.createElement('div');
-counter_container.setAttribute('class', 'counter_container')
-container.appendChild(counter_container)
+var counter_container = document.createElement("div");
+counter_container.setAttribute("class", "counter_container");
+container.appendChild(counter_container);
 
-var displayed_number = document.createElement('p')
-displayed_number.setAttribute('class', 'current_number')
-counter_container.appendChild(displayed_number)
-
+var displayed_number = document.createElement("p");
+displayed_number.setAttribute("class", "current_number");
+counter_container.appendChild(displayed_number);
+displayed_number.innerHTML = /*"Press Start"*/ current_number;
 
 //Create a button container and add buttons
-var buttons = document.createElement('div')
-buttons.setAttribute('class', 'button_container')
-container.appendChild(buttons)
+var buttons = document.createElement("div");
+buttons.setAttribute("class", "button_container");
+container.appendChild(buttons);
 
-var startbtn = document.createElement('button')
-var stopbtn = document.createElement('button')
-var resetbtn = document.createElement('button')
+var startbtn = document.createElement("button");
+var stopbtn = document.createElement("button");
+var resetbtn = document.createElement("button");
 
-startbtn.innerText = "Start!"
-stopbtn.innerText = "Stop!"
-resetbtn.innerText = "Reset."
-buttons.appendChild(startbtn)
-buttons.appendChild(stopbtn)
-buttons.appendChild(resetbtn)
+startbtn.innerText = "Start!";
+stopbtn.innerText = "Stop!";
+resetbtn.innerText = "Reset.";
+buttons.appendChild(startbtn);
+buttons.appendChild(stopbtn);
+buttons.appendChild(resetbtn);
 
-var interval
-
-
+var interval;
+var minutes = 0;
+var seconds = 0;
 
 // const minute = 1000 * 60;
 // const hour = minute * 60;
@@ -49,21 +49,50 @@ var interval
 
 //Create a timer to start counting when start button is pressed
 
-current_number = 00
+startbtn.onclick = function () {
+  clearInterval(interval);
+  interval = setInterval(adder, 10);
+};
 
-startbtn.onclick =function() {
-    clearInterval(interval)
-    interval = setInterval(adder, 10)
+stopbtn.onclick = function () {
+  clearInterval(interval);
+};
+resetbtn.onclick = function () {
+  clearInterval(interval);
+  current_number = 00;
+  seconds = 00;
+  minutes = 00;
+  displayed_number.innerHTML = "Cleared";
+};
+
+function adder() {
+  current_number++;
+
+  if (current_number > 99) {
+    current_number = 00;
+    seconds++;
+  }
+  if (seconds > 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  if (current_number < 9) {
+    displaymillis = "0" + current_number;
+  } else {
+    displaymillis = current_number;
+  }
+  if (seconds < 9) {
+    displayseconds = "0" + seconds;
+  } else {
+    displayseconds = seconds;
+  }
+  if (minutes < 9) {
+    displayminutes = "0" + minutes;
+  } else {
+    displayminutes = minutes;
+  }
+
+  displayed_number.innerHTML =
+    displayminutes + " " + displayseconds + " " + displaymillis;
 }
-
-stopbtn.onclick = function(){
-    clearInterval(interval)
-}
-
-function adder() { 
-    current_number ++
-    displayed_number.innerHTML = current_number
-}
-
-
-
